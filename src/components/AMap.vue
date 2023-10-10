339a99600e80626f697c8792d3e4b31d32618a33
mrchar <mrchar@qq.com>
2023-10-10 11:03:59 +0800

    引入缩放和比例尺控件

--- src/components/AMap.vue
+++ src/components/AMap.vue
@@ -33,7 +33,7 @@ function initMap() {
   AMapLoader.load({
     key: amapKey, // 申请好的Web端开发者Key，首次调用 load 时必填
     version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-    plugins: [""], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+    plugins: ["AMap.ToolBar", "AMap.Scale"], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
   })
     .then((AMap) => {
       map = new AMap.Map(
@@ -44,6 +44,10 @@ function initMap() {
           center: [105.602725, 37.076636], //初始化地图中心点位置
         },
       )
+
+      // 添加控件
+      map!.addControl(new AMap.ToolBar())
+      map!.addControl(new AMap.Scale())
 
       // 绑定事件
       map!.on("click", clickHandler)
